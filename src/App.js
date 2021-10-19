2061a16810777c26f78be0648f10265ccd82aa2a
Kenny Bates <kennyleebates@outlook.com>
2021-10-19 19:26:59 -0400

    Create Setup
    Created initial files, components, and CSS needed for food menu.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import Menu from './Menu';
+import Categories from './Categories';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Menu</h2>
     </div>
   );
 }
